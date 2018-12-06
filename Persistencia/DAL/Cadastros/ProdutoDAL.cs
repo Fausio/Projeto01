b2ed2302340f33c1e0572be76001412b36d704f7
fausio luis <fausioluis@live.com>
2018-12-06 04:42:00 -0800

    adding methot to save, edit, list and delete produtos in produtoDAL

--- Persistencia/DAL/Cadastros/ProdutoDAL.cs
+++ Persistencia/DAL/Cadastros/ProdutoDAL.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Persistencia.Contexts;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+using Modelo.Cadastros;
 
 namespace Persistencia.DAL.Cadastros
 {
-    class ProdutoDAL
+    public class ProdutoDAL
     {
+        private EFContext context = new EFContext();
+        public IQueryable<Produto> ObterProdutosClassificadosPorNome()
+        {
+            return context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).OrderBy(n => n.Nome);
+        }
+
+
+        public Produto ObterProdutoPorId(long id)
+        {
+            return context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+        }
+
+
+
+        //    nesse metodo se o produto vier sem ID ele vai encarar como um registro novo,
+        //    porem se ele vir com um ID ele vai encarar como um produto por se atualizar
+        public void GravarProduto(Produto produto)
+        {
+            if (produto.ProdutoId == null)
+            {
+                context.Produtos.Add(produto);
+            }
+            else
+            {
+                context.Entry(produto).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        }
+
+
+        public Produto EliminarProdutoPorId(long id)
+        {
+            Produto produto = ObterProdutoPorId(id);
+            context.Produtos.Remove(produto);
+            context.SaveChanges();
+            return produto;
+        }
     }
 }
