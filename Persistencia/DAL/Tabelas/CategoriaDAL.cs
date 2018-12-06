a3984c581cb86976c967fc48a22cc384a2ef12e0
fausio luis <fausioluis@live.com>
2018-12-06 04:29:54 -0800

    Adding FabricanteDAL

--- Persistencia/DAL/Tabelas/CategoriaDAL.cs
+++ Persistencia/DAL/Tabelas/CategoriaDAL.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Persistencia.Contexts;
 
 namespace Persistencia.DAL.Tabelas
 {
     public class CategoriaDAL
     {
         private EFContext context = new EFContext();
-        public IQueryable<Categoria>
-        ObterCategoriasClassificadasPorNome()
+        public IQueryable<Categoria> ObterCategoriasClassificadasPorNome()
         {
             return context.Categorias.OrderBy(b => b.Nome);
         }
