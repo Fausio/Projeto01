6d56c0109f86e11ce26b9241240f5ff225ca41ec
fausio luis <fausioluis@live.com>
2018-12-06 04:19:33 -0800

    Creating  CategoriaDAL (list)

---
+++ Persistencia/DAL/Tabelas/CategoriaDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.DAL.Tabelas
+{
+    public class CategoriaDAL
+    {
+        private EFContext context = new EFContext();
+        public IQueryable<Categoria>
+        ObterCategoriasClassificadasPorNome()
+        {
+            return context.Categorias.OrderBy(b => b.Nome);
+        }
+    }
+}
