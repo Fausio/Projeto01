fb8b6f2fa1cde19193c1f8603ab2b1527692cb48
fausio luis <fausioluis@live.com>
2019-12-03 14:37:15 -0800

    adding two controlers (categoria and fabricante)

---
+++ projeto01/Controllers/CategoriasController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto01.Context;
+using Projeto01.Models;
+
+namespace Projeto01.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+
+            return View(context.categorias.ToList());
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(context.categorias.Where(x => x.CategoriaId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+
+            if (ModelState.IsValid)
+            {
+                context.Entry(categoria).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("categoria");
+            }
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(context.categorias.Where(x => x.CategoriaId == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(context.categorias.Where(x => x.CategoriaId == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+
+            context.categorias.Remove(context.categorias.Where(x => x.CategoriaId == categoria.CategoriaId).First());
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.categorias.Add(categoria);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(categoria);
+        }
+
+    }
+}
---
+++ projeto01/Controllers/FabrincatesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Projeto01.Context;
+using Projeto01.Models;
+
+namespace Projeto01.Controllers
+{
+    public class FabrincatesController : Controller
+    {
+        EFContext context = new EFContext();
+
+        // GET: Fabrincates
+        public ActionResult Index()
+        {
+            return View(context.fabricantes.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.fabricantes.Add(fabricante);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(fabricante);
+            }
+
+        }
+
+        public ActionResult Edit(long? Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (context.fabricantes.Find(Id) == null)
+            {
+                return HttpNotFound();
+            }
+            return View(context.fabricantes.Find(Id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(fabricante);
+            }
+
+        }
+
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (context.fabricantes.Find(id) == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(context.fabricantes.Find(id));
+
+        }
+
+        public ActionResult Delete(long? id)
+        {
+
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            if (context.fabricantes.Find(id) == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(context.fabricantes.Find(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Fabricante fabricante)
+        {
+
+
+            context.fabricantes.Remove(context.fabricantes.Find(fabricante.FabricanteID));
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
