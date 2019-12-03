571225b47042d56dc16b1bb69dc0e527e08a70eb
fausio luis <fausioluis@live.com>
2019-12-03 15:31:14 -0800

    adding Jquey databales

--- projeto01/App_Start/RouteConfig.cs
+++ projeto01/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace projeto01
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Categorias", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Fabrincates", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
