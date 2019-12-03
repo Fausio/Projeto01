4e31b9d070d85454ee937983ac4a9e14c8ea3de7
fausio luis <fausioluis@live.com>
2019-12-03 14:37:52 -0800

    adding EFContext

---
+++ projeto01/Context/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Projeto01.Models;
+
+namespace Projeto01.Context
+{
+    public class EFContext : DbContext
+    {
+        public DbSet<Fabricante> fabricantes { get; set; }
+        public DbSet<Categoria> categorias { get; set; }
+    }
+}
