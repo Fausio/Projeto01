d4bbf9eef665e0b2360a6838334aeaeb67ca1ef4
fausio luis <fausioluis@live.com>
2019-12-03 14:36:09 -0800

    adding two models (categoria and fabricante)

---
+++ projeto01/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto01.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ projeto01/Models/Fabricante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto01.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteID { get; set; }
+        public string Nome { get; set; }
+    }
+}
