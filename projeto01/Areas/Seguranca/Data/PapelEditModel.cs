ea3f1210778d0a66775fed4484f613226b42edef
fausio luis <fausioluis@live.com>
2019-12-24 00:34:38 +0200

    PapelEditModel , PapelModificationModel

---
+++ projeto01/Areas/Seguranca/Data/PapelEditModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace projeto01.Areas.Seguranca.Data
+{
+    public class PapelEditModel
+    {
+        public Papel Papel { get; set; }
+        public IEnumerable<Usuario> Membros { get; set; }
+        public IEnumerable<Usuario> NaoMembros { get; set; }
+    }
+
+
+}
