d8ada9321f07131dd6bacf54c084e2804d4870ee
fausio luis <fausioluis@live.com>
2019-12-23 11:01:22 +0200

    adding area Usuario model and  IdentityDbContextAplicacao

---
+++ projeto01/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using projeto01.Areas.Seguranca.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Projeto01.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb")
+        {
+        }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+    }
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao>
+    {
+
+    }
+}
