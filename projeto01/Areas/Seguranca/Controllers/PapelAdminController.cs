f01e23956714026110b6e3cc666c32da6d9e6836
fausio luis <fausioluis@live.com>
2019-12-24 00:34:09 +0200

    public ActionResult Edit

--- projeto01/Areas/Seguranca/Controllers/PapelAdminController.cs
+++ projeto01/Areas/Seguranca/Controllers/PapelAdminController.cs
@@ -5,6 +5,9 @@ using projeto01.Areas.Seguranca.Data;
 using System.Web;
 using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
 
 namespace projeto01.Areas.Seguranca.Controllers
 {
@@ -63,6 +66,52 @@ namespace projeto01.Areas.Seguranca.Controllers
                 }
             }
             return View(nome);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            Papel papel = RoleManager.FindById(id);
+            string[] memberIDs = papel.Users.Select(x => x.UserId).ToArray();
+
+            IEnumerable<Usuario> membros = UserManager.Users.Where(x => memberIDs.Any(y => y == x.Id));
+            IEnumerable<Usuario> naoMembros = UserManager.Users.Except(membros);
+
+            return View(new PapelEditModel
+            {
+                Papel = papel,
+                Membros = membros,
+                NaoMembros = naoMembros
+            });
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(PapelModificationModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.IdsParaAdicionar ?? new string[] { })
+                {
+                    result = UserManager.AddToRole(userId, model.NomePapel);
+
+                    if (!result.Succeeded)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                }
+
+                foreach (string userId in model.IdsParaRemover ?? new string[] { })
+                {
+                    result = UserManager.RemoveFromRole(userId, model.NomePapel);
+                    if (!result.Succeeded)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         }
     }
 }
