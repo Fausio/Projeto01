2ddea335ca03f8787bc611dd28db4056a552b5ca
fausio luis <fausioluis@live.com>
2019-12-27 17:14:47 +0200

    add CarrinhosController

---
+++ projeto01/Areas/Carrinho/Controllers/CarrinhosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projeto01.Areas.Carrinho.Controllers
+{
+    public class CarrinhosController : Controller
+    {
+        // GET: Carrinho/Carrinhos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
