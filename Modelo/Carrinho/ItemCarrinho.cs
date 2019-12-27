9becdf2cea7bf8e8854ac78fd500e0de45a0673f
fausio luis <fausioluis@live.com>
2019-12-27 17:10:26 +0200

    adding model ItemCarrinho

---
+++ Modelo/Carrinho/ItemCarrinho.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Cadastros;
+
+namespace Modelo.Carrinho
+{
+   public class ItemCarrinho
+    {
+        public long? ItemCarrinhoId { get; set; }
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+        public double ValorUnitario { get; set; }
+
+        public double SubTotal
+        {
+            get
+            {
+                return Quantidade * ValorUnitario;
+            }
+        }
+    }
+}
