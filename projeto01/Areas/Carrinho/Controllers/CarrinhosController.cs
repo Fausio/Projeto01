6cfa6d552791267c3e8dccccec580b4cc226ab51
fausio luis <fausioluis@live.com>
2019-12-27 17:19:44 +0200

    adding actionresult create in carrinhoscontroller

--- projeto01/Areas/Carrinho/Controllers/CarrinhosController.cs
+++ projeto01/Areas/Carrinho/Controllers/CarrinhosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Modelo.Carrinho;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,21 @@ namespace projeto01.Areas.Carrinho.Controllers
     public class CarrinhosController : Controller
     {
         // GET: Carrinho/Carrinhos
-        public ActionResult Index()
+        public ActionResult Create()
         {
-            return View();
+
+            //inicia recuperando uma variável chamada carrinho da sessão existente no contexto da requisição HTTP.
+            IEnumerable<ItemCarrinho> carrinho = HttpContext.Session["carrinho"] as IEnumerable<ItemCarrinho>;
+
+
+            //Caso a variável seja nula, significa que está ocorrendo a primeira requisição do
+            //cliente para esta action / visão, o que leva, então, a criação da variável na sessão,  antes de retorná-la.
+            if (carrinho == null)
+            {
+                carrinho = new List<ItemCarrinho>();
+                HttpContext.Session["carrinho"] = carrinho;
+            }
+            return View(carrinho);
         }
     }
 }
